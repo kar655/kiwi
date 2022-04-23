f0d7c190812b58b5926f566e7b8dc365956d1339
kar655 <karol123zag@gmail.com>
2022-04-23 14:11:06 +0200

    Fetch event lists

--- events.py
+++ events.py
@@ -4,3 +4,6 @@ from eventbrite import Eventbrite
 class Events:
     def __init__(self, api_key):
         self.eventbrite = Eventbrite(api_key)
+
+    def get_event_list(self, event_ids):
+        return [self.eventbrite.get_event(e_id) for e_id in event_ids]
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     api_key = 'A6FCWAHLHYF6EKU5HF2W'
     events = Events(api_key)
     pprint(events.eventbrite.get_user())
+    event_ids = ['320787663537']
+    pprint(events.get_event_list(event_ids))
 
 
 if __name__ == '__main__':
